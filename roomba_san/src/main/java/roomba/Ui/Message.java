f5b4fa049e17ec7685d559348a6f342403e5946a
proto-aiken-13 <120178555+proto-aiken-13@users.noreply.github.com>
2023-12-05 22:25:21 +0800

    Add Message file

---
+++ roomba_san/src/main/java/roomba/Ui/Message.java
@@ -0,0 +1,14 @@
+package roomba.Ui;
+
+/**
+ * The auxiliary enumeration to show all messages.
+ */
+public class Message {
+    public static final String GREETING_MESSAGE = "Helloings! I'm Roomba! How can I help you today?";
+    
+    public static final String COMMAND_ERROR_MESSAGE = "Uhhh" + "\n"
+                                                        + "Idk" + "\n"
+                                                        + "LOL";
+    
+    public static final String MISC = "Nyaaarrrhhh";
+}
